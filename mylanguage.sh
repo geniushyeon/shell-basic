a1805ee08cecccbf4c98976229cc9c1b5b3dd8ac
geniushyeon <jhlee0912@icloud.com>
2021-07-17 23:48:35 +0900

    📝✨ 확장 변경자 학습 중

--- mylanguage.sh
+++ mylanguage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# echo "This shell script name is $0"
-# echo "I can speak $1 and $2"
-# echo "This shell script parameters are $*"
-# echo "This shell script parameters are $@"
-# echo "This parameter count is $#"
+echo "This shell script name is $0"
+echo "I can speak $1 and $2"
+echo "This shell script parameters are $*"
+echo "This shell script parameters are $@"
+echo "This parameter count is $#"
 
 for language in "$@"
 do
